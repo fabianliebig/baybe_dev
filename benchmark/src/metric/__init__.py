eb97765cdb2e24fa4d36fed4290c4988ce206706
Fabian Liebig <fabian.liebig@merckgroup.com>
2024-10-22 14:58:20 +0200

    Add measurement base. Add a base class for upcoming measurement implementation to reflect them in the benchmarking classes already.

---
+++ benchmark/src/metric/__init__.py
@@ -0,0 +1,11 @@
+"""Metrics for evaluating the performance of algorithms."""
+
+from benchmark.src.metric.base import (
+    Metric,
+    NormalizationInterface,
+)
+
+__all__ = [
+    "Metric",
+    "NormalizationInterface",
+]
