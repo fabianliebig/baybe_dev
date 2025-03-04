b21d5c90df8e4a42d75ee54dd61cadc2fa4130b6
Fabian Liebig <fabian.liebig@merckgroup.com>
2025-03-04 19:53:44 +0100

    Refactor benchmarking workflow and add support for specific benchmark execution and matrix usage

--- benchmarks/__main__.py
+++ benchmarks/__main__.py
@@ -1,27 +1,57 @@
 """Executes the benchmarking module."""
 # Run this via 'python -m benchmarks' from the root directory.
 
+import argparse
 import os
 
+from benchmarks.definition import Benchmark
 from benchmarks.domains import BENCHMARKS
 from benchmarks.persistence import (
     LocalFileObjectStorage,
     PathConstructor,
     S3ObjectStorage,
 )
+from benchmarks.result import Result
 
 RUNS_IN_CI = "CI" in os.environ
 
 
-def main():
-    """Run all benchmarks."""
-    for benchmark in BENCHMARKS:
-        result = benchmark()
-        path_constructor = PathConstructor.from_result(result)
-        persist_dict = benchmark.to_dict() | result.to_dict()
+def save_benchmark_data(benchmark: Benchmark, result: Result):
+    """Save the benchmark data to the object storage."""
+    path_constructor = PathConstructor.from_result(result)
+    persist_dict = benchmark.to_dict() | result.to_dict()
 
-        object_storage = S3ObjectStorage() if RUNS_IN_CI else LocalFileObjectStorage()
-        object_storage.write_json(persist_dict, path_constructor)
+    object_storage = S3ObjectStorage() if RUNS_IN_CI else LocalFileObjectStorage()
+    object_storage.write_json(persist_dict, path_constructor)
+
+
+def run_all_benchmarks():
+    """Run all benchmarks."""
+    for benchmark in BENCHMARKS.keys():
+        result = benchmark()
+        save_benchmark_data(benchmark, result)
+
+
+def run_specific_benchmarks(benchmark_subset_names: list[str]):
+    """Run a subset based on the benchmark name."""
+    for benchmark_name in benchmark_subset_names:
+        benchmark = BENCHMARKS[benchmark_name]
+        result = benchmark()
+        save_benchmark_data(benchmark, result)
+
+
+def main():
+    """Execute the benchmarking module."""
+    parser = argparse.ArgumentParser(description="Executes the benchmarking module.")
+    parser.add_argument(
+        "--benchmark-list", nargs="+", help="List of benchmarks to run", default=None
+    )
+    args = parser.parse_args()
+    if not args.benchmark_list:
+        run_all_benchmarks()
+        return
+
+    run_specific_benchmarks(args.benchmark_list)
 
 
 if __name__ == "__main__":
--- benchmarks/domains/__init__.py
+++ benchmarks/domains/__init__.py
@@ -3,8 +3,8 @@
 from benchmarks.definition.base import Benchmark
 from benchmarks.domains.synthetic_2C1D_1C import synthetic_2C1D_1C_benchmark
 
-BENCHMARKS: list[Benchmark] = [
-    synthetic_2C1D_1C_benchmark,
-]
+BENCHMARKS: dict[str, Benchmark] = {
+    "synthetic_2C1D_1C": synthetic_2C1D_1C_benchmark,
+}
 
 __all__ = ["BENCHMARKS"]
