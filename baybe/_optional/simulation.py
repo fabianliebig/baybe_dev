8e79b51177ef09e0040e3903a2e8cdac6f87361f
Fabian Liebig <fabian.liebig@merckgroup.com>
2025-08-28 15:08:43 +0200

    Add support for parallel execution by setting joblib's start method to 'spawn'

--- baybe/_optional/simulation.py
+++ baybe/_optional/simulation.py
@@ -4,6 +4,9 @@ from baybe.exceptions import OptionalImportError
 
 try:
     import xyzpy
+    from joblib.externals.loky.backend.context import set_start_method
+    set_start_method("spawn", force=True)
+
 except ModuleNotFoundError as ex:
     raise OptionalImportError(name="xyzpy", group="simulation") from ex
 
